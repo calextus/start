b5c2fb3254a581612e2d3ef18fcd86094a9d91e1
Selimo Shano <mokherhi@gmail.com>
2024-02-27 15:30:44 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css'
 
+const cardImages = [
+  {"src": "/img/helmet.jpg" },
+  {"src": "/img/potion.jpg" },
+  {"src": "/img/ring.jpg" },
+  {"src": "/img/scroll.jpg" },
+  {"src": "/img/shield.jpg" },
+  {"src": "/img/sword.jpg" }, 
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+// shuffle cards
+const shufflecards = () => {
+  const shuffledcards = [...cardImages, ...cardImages]
+     .sort(() => Math.random() - 0.5)
+     .map((card) => ({...card, id: Math.random() }))
+
+     setCards(shufflecards) 
+     setTurns(0)
+}
+
+
+  console.log(cards, turns)
+
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Games</button>
+      <button onClick={shufflecards}>New Games</button>
     </div>
   );
 }
